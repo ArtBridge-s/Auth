80c4cf0b2c7d5a1f9694761a5fb7159f346a17cb
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-01 11:39:29 +0900

    docs(BC-20): Update MemberNameProducer interface documentation

--- src/main/java/com/artbridge/gateway/adapter/MemberNameProducer.java
+++ src/main/java/com/artbridge/gateway/adapter/MemberNameProducer.java
@@ -1,5 +1,13 @@
 package com.artbridge.gateway.adapter;
 
+/**
+ * 회원 이름을 처리하는 인터페이스입니다.
+ */
 public interface MemberNameProducer {
+    /**
+     * 회원 이름을 처리합니다.
+     *
+     * @param name 처리할 회원 이름 (String)
+     */
     void memberNameProduce(String name);
 }
